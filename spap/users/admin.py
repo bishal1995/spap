b74a292b7a6b9b213ad6cbd8158de98e21bb799a
Bishal Paul <bishalpaul1905@gmail.com>
2016-10-01 09:41:01 +0000

    User INFO added

--- spap/users/admin.py
+++ spap/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import LastActivity,DivisionOfficer,RangeOfficer,BeatOfficer
 # Register your models here.
+admin.site.register(User)
 admin.site.register(LastActivity)
 admin.site.register(DivisionOfficer)
 admin.site.register(RangeOfficer)
